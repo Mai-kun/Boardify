9242d528a38a9171a4473c38f2717a7ec4788419
Mai-kun <amaz1ngcreature10@mail.ru>
2025-06-25 19:52:45 +0300

    Add localization on English

---
+++ BoardifyApp/Helpers/LocExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Resources;
+using System.Windows.Markup;
+
+namespace BoardifyApp.Helpers;
+
+public class LocExtension(string key) : MarkupExtension
+{
+    private static readonly ResourceManager ResourceManager = Resources.en_lang.ResourceManager;
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        var culture = CultureInfo.CurrentUICulture;
+        return ResourceManager.GetString(key, culture) ?? $"!{key}!";
+    }
+}
